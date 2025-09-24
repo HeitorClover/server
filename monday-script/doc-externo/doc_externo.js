0ea4cd1aee029d299b04ef6d75d490c7fd22bcb6
HeitorClover <153210142+HeitorClover@users.noreply.github.com>
2025-09-24 16:34:34 -0300

    Add files via upload

--- monday-script/doc-externo/doc_externo.js
+++ monday-script/doc-externo/doc_externo.js
@@ -324,7 +324,7 @@ async function processWebhookEvent(body) {
     const itemName = item.name.toUpperCase();
     const isUnificacaoIniciada = itemName.includes('UNIFICAÇÃO INICIADA');
     const isDesmembramentoIniciado = itemName.includes('DESMEMBRAMENTO INICIADO');
-    const isProjIniciado = itemName.includes('PROJ INICIADO');
+    const isProjIniciado = itemName.includes('PROJETO INICIADO');
     const isDocEmitirAlvara = itemName.includes('DOC - EMITIR ALVARÁ');
 
     if (!isUnificacaoIniciada && !isDesmembramentoIniciado && !isProjIniciado && !isDocEmitirAlvara) {
